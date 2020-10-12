7c9825e27c52d15bbfaef4f7bdbf89b21a30de4c
FarzanaTonny <54808958+FarzanaTonny@users.noreply.github.com>
2020-10-13 02:44:26 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+    new WOW().init();
+
+    // Swiper demo
+
+    // var swiper = new Swiper('.swiper-container', {
+    //   slidesPerView: 2.5,
+    //   spaceBetween: 30,
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   },
+    //   // scrollbar: {
+    //   //   el: '.swiper-scrollbar',
+    //   //   hide: true,
+    //   // },
+    // });
+
+    var swiper = new Swiper('.swiper-container', {
+    	slidesPerView: 2,
+        spaceBetween: 30,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+      },
+    });
